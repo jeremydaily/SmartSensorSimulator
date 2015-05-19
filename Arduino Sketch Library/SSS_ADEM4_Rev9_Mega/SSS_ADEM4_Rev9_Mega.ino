d76cc9bb1a77a8909419ec272b44fd6e3bbec6c4
jose-corcega <jose-corcega@utulsa.edu>
2015-05-18 21:07:38 -0500

    cat adem 4
    Made changes in order to compile this code. These are the correct values
    needed to get a fault free ADEM 4 ECM.

--- Arduino Sketch Library/SSS_ADEM4_Rev9_Mega/SSS_ADEM4_Rev9_Mega.ino
+++ Arduino Sketch Library/SSS_ADEM4_Rev9_Mega/SSS_ADEM4_Rev9_Mega.ino
@@ -38,10 +38,7 @@ the first nibble of the CAN message tells the SSS where to put the message.
 #include <Mcp4261.h>
 #include <SSS.h>
 
-SSS sss = SSS(100); // Call the Smart Sensor Simulator library. 
-char command[100]; //declare the array to store serial command characters.
-
-String IDstring = "SYNER*SSS-ECMmake*ABCDEFG    ";
+String IDstring = "SYNER*SSS-ADEM4*1R90003     ";// this string must contain XX characters
 
 void setup()
 { 
@@ -57,33 +54,33 @@ void setup()
   sss.settings[0] = 50; //U1-P0W, J16-9, P2-38, Air Inlet Temp, Green/White
   sss.settings[1] = 50; //U1-P1W, J16-10, P2-84, Coolant Temp, Red/Blue
   sss.settings[2] = 50; //U1-P2W, J16-11, P2-95, Inlet Manifold Air Temp, Brown/Blue
-  sss.settings[3] = 50; //U1-P3W, J16-13, P2-96, Fuel Temp, Yellow/White
+  sss.settings[3] = 10; //U1-P3W, J16-13, P2-96, Fuel Temp, Yellow/White
   sss.settings[4] = 50; //U2-P0W, J18-2, P1-49, Coolant level Norm (4 wire), Grey/Black
-  sss.settings[5] = 50; //U2-P1W, J18-3, P1-26, Coolant level normal (2 wire), White/Black
-  sss.settings[6] = 50; //U2-P2W, J18-6, P1-19, Output # 6, Yellow/Black
-  sss.settings[7] = 50; //U2-P3W, J18-7, P1-20, Output # 7, Green/Black
+  sss.settings[5] = 00; //U2-P1W, J18-3, P1-26, Coolant level normal (2 wire), White/Black
+  sss.settings[6] = 00; //U2-P2W, J18-6, P1-19, Output # 6, Yellow/Black
+  sss.settings[7] = 00; //U2-P3W, J18-7, P1-20, Output # 7, Green/Black
   sss.settings[8] = 50; //U3-P0W, J10-4, P2-5, Coolant Diverter Solenoid/ADR Valve, Tan
-  sss.settings[9] = 50; //U3-P1W, J10-9, P2-41, ARD Pilot Fuel Pressure, Pink/Black
-  sss.settings[10] = 50; //U3-P2W, J10-5, P2-40, Fuel Pressure, Tan/White
+  sss.settings[9] = 30; //U3-P1W, J10-9, P2-41, ARD Pilot Fuel Pressure, Pink/Black
+  sss.settings[10] = 55; //U3-P2W, J10-5, P2-40, Fuel Pressure, Tan/White
   sss.settings[11] = 50; //U3-P3W, J10-10, P2-89, ARD Fuel Pressure, White/Black
   sss.settings[12] = 50; //U4-P0W, J24-15, P2-13, DPF inlet temperature (Double), Blue
   sss.settings[13] = 50; //U4-P1W, J24-16, P2-62, DPF outlet temperature (Double), Brown/White
   sss.settings[14] = 50; //U4-P2W, J24-17, P2-56, DPF inlet temperature (Single), Orange/White
   sss.settings[15] = 50; //U4-P3W, J24-18, P2-94, DPF outlet temperature (Single), Purple/Black
   sss.settings[16] = 50; //U5-P0W, J10-7,  P2-27, ARD MAF Delta Pressure, Brown
-  sss.settings[17] = 50; //U5-P1W, J24-9,  P2-66, DPF Pressure Sensor (Single), Green/Black
-  sss.settings[18] = 50; //U5-P2W, J24-10, P2-85, DPF pressure Sensor (Double), Blue/Black
-  sss.settings[19] = 50; //U5-P3W, J24-14, P2-57, CGI Absolute pressure, Purple/White
+  sss.settings[17] = 07; //U5-P1W, J24-9,  P2-66, DPF Pressure Sensor (Single), Green/Black
+  sss.settings[18] = 10; //U5-P2W, J24-10, P2-85, DPF pressure Sensor (Double), Blue/Black
+  sss.settings[19] = 20; //U5-P3W, J24-14, P2-57, CGI Absolute pressure, Purple/White
   
   //The following settings are in milliVolts
-  sss.settings[20] = 1000; //VoutA, J24-19, P2-28, Oil Press, Red/White
+  sss.settings[20] = 2500; //VoutA, J24-19, P2-28, Oil Press, Red/White
   sss.settings[21] = 2000; //VoutB, J24-20, P2-33, Crank Case Pressure, Black/White
   sss.settings[22] = 3000; //VoutC, J24-21, P2-15, Inlet Manifold Air Pressure, Orange
-  sss.settings[23] = 4000; //VoutD, J24-22, P2-74, Intake Valve actuation oil  Press, Tan/Black
+  sss.settings[23] = 2000; //VoutD, J24-22, P2-74, Intake Valve actuation oil  Press, Tan/Black
   
   //The following settings are in duty cycle, so 50 = 2.5V average
   sss.settings[24] = 50; //PWM1, J24-23, P2-16, CGI Delta Pressure, Purple
-  sss.settings[25] = 50; //PWM2,    (Not Connected on Rev 10. May need to move to E2WS1)
+  sss.settings[25] = 35; //PWM2,    (Not Connected on Rev 10. May need to move to E2WS1)
     
   // for the following switch settings 0 = connected (closed) and 1 = disconnected (open).
   // These are normally closed switches. Uses 120 ohm resistor
@@ -112,17 +109,17 @@ void setup()
   sss.settings[44] = 0; //VccSelectU2-2
   sss.settings[45] = 0; //VccSelectU2-3
   sss.settings[46] = 0; //VccSelectU3-0
-  sss.settings[47] = 0; //VccSelectU3-1
+  sss.settings[47] = 1; //VccSelectU3-1
   sss.settings[48] = 0; //VccSelectU3-2
-  sss.settings[49] = 0; //VccSelectU3-3
+  sss.settings[49] = 1; //VccSelectU3-3
   sss.settings[50] = 0; //VccSelectU4-0
   sss.settings[51] = 0; //VccSelectU4-1
   sss.settings[52] = 0; //VccSelectU4-2
   sss.settings[53] = 0; //VccSelectU4-3
   sss.settings[54] = 0; //VccSelectU5-0
   sss.settings[55] = 0; //VccSelectU5-1
-  sss.settings[56] = 0; //VccSelectU5-2
-  sss.settings[57] = 0; //VccSelectU5-3
+  sss.settings[56] = 1; //VccSelectU5-2
+  sss.settings[57] = 1; //VccSelectU5-3
  
   //For the following switches on the low side of the potentiometer, 0 = connected to ground, 1 = disconnected.
   sss.settings[58] = 0; //GroundSelectU1-0
@@ -143,7 +140,7 @@ void setup()
   sss.settings[73] = 0; //GroundSelectU4-3
   sss.settings[74] = 0; //GroundSelectU5-0
   sss.settings[75] = 0; //GroundSelectU5-1
-  sss.settings[76] = 0; //GroundSelectU5-2
+  sss.settings[76] = 1; //GroundSelectU5-2
   sss.settings[77] = 0; //GroundSelectU5-3
   
   //CAN2 Panel Select - 0 = CAN2 on 9-pin is connected to E-CAN, 1 = disconnected
@@ -174,12 +171,12 @@ void setup()
   else Serial.println("CAN3 init fail!!");
   
   String commandString = "CAN18FEF521100000000000000000000"; // PGN 65269 Ambient Conditions 1000 = 1 second period
-  commandString.toCharArray(command,32);
+  commandString.toCharArray(sss.command,32);
   sss.processCommand(31);
   
   IDstring.toCharArray(sss.compID,29);
 
-  for (int h = 0; h<83; h++) sss.adjustSetting(h);
+  for (int h = 0; h<83; h++) adjustSetting(h);
    
   
   Serial.println("Finished Starting Up... Type a command:");
