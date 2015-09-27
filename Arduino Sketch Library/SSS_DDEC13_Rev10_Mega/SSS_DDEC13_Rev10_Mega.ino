4e43c56258d5c922f8be182f13a470ad78edf1af
jeremy-daily <jeremy@synercontechnologies.com>
2015-09-27 13:37:59 -0500

    DDEC 13 CPC4 Only Sketches are Fault Free
    Also include PACCAR sketch starts.

--- Arduino Sketch Library/SSS_DDEC13_Rev10_Mega/SSS_DDEC13_Rev10_Mega.ino
+++ Arduino Sketch Library/SSS_DDEC13_Rev10_Mega/SSS_DDEC13_Rev10_Mega.ino
@@ -10,11 +10,32 @@ Comments are for the wiring of an SSS for a DDEC 10.
 #include <Mcp4261.h> // Digital Potentiometers
 #include <SSS.h> // Pin definitions and helper functions
 
+#define LINcsPIN 4
+#define LINwakePIN 85
+
+unsigned long currentMicros;
+unsigned long previousMicros;
+unsigned long serialSend0Micros;
+unsigned long serialSend1Micros;
+unsigned long serialSend2Micros;
+unsigned long serialSend3Micros;
+unsigned long serialSend4Micros;
+
+byte LINDataBytes[3];
+int i;
+byte k;
+byte outByte[5];
+boolean firstLINtime=true;
+boolean LIN0send = false;
+boolean LIN1send = false;
+boolean LIN2send = false;
+boolean LIN3send = false;
+boolean LIN4send = false;
 
 void setup()
 { 
   sss.begin();
-  sss.IDstring = "SYNER*SSS-DDECX*1R90004     "; //Change this to match the case of the SSS you are programming. Be sure to include trailing spaces.
+  sss.IDstring = "SYNER*SSS-DDEC13*1R90004     "; //Change this to match the case of the SSS you are programming. Be sure to include trailing spaces.
   sss.IDstring.toCharArray(sss.compID,29); // Convert the component ID to a character array for CAN
 
   //Adjust the settings below to match the particular Smart Sensor Simulator that is being programmed. These settings are interpreted by the adjustSetting(i) function in SSS.cpp
@@ -23,7 +44,7 @@ void setup()
   sss.settings[1] = 15;  //w U1-P1W (J16-10): MCM120-109, Differential pressure EGR Yellow/White
   sss.settings[2] = 20;  //e U1-P2W (J16-11): MCM120-110, Engine Coolant Outlet Temperature, Grey/Black, Fault 110
   sss.settings[3] = 10;  //r U1-P3W (J16-13): MCM120-57, Water level for fuel water separator, Grey
-  sss.settings[4] = 40;  //t U2-P0W (J18-2): CPC1-4, Accelerator Pedal Position, Tan/Black, 
+  sss.settings[4] = 3;  //t U2-P0W (J18-2): CPC1-4, Accelerator Pedal Position, Tan/Black, 
   sss.settings[5] = 55;  //y U2-P1W (J18-3): CPC3-5, Multifunction, Grey/White, Open 
   sss.settings[6] = 20;   //u U2-P2W (J18-6): CPC4-14, Throttle Position Sensor 2, Purple/White
   sss.settings[7] = 50;  //i U2-P3W (J18-7): CPC1-1, Multifunction, Brown, Open circuit
@@ -156,12 +177,70 @@ void setup()
   commandString.toCharArray(sss.command,32);
   sss.processCommand(31);
   
+  commandString = "CAN18F0013101000000000000000000"; // Electronic Brake Controller from SA=49 100 = .1 second period
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18F0010B01000000000000000000"; // Electronic Brake Controller from SA=11 100 = .1 second period
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18FEF11701000000000000000000"; //CCVS from SA=23
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+ 
+  commandString = "CAN18FEF12801000000000000000000"; //CCVS from SA=40
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18FEF12101000000000000000000"; //CCVS from SA=33
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18FEF13101000000000000000000"; //CCVS from SA=49
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18E0001701000000000000000000"; //Cab Message 1 CM1  from SA=23
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18E0001901000000000000000000"; //Cab Message 1 CM1  from SA=25
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18E0002101000000000000000000"; //Cab Message 1 CM1  from SA=33
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18E0002801000000000000000000"; //Cab Message 1 CM1  from SA=40
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN18E0003101000000000000000000"; //Cab Message 1 CM1  from SA=49
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
+  
+  commandString = "CAN0CFE6E0B00200000000000000000";// High Resolution Wheel Speed  message from Brake Controller
+  commandString.toCharArray(sss.command,32);
+  sss.processCommand(31);
   
   for (int h = 0; h < sss.numCommands; h++) adjustSetting(h);
   Serial.println("Main Board Program for ATmega2560 Processor.");
   Serial.println(sss.IDstring);
-  Serial.println("Finished Starting Up... Type a command:");
   
+  pinMode(LINcsPIN,OUTPUT);
+  pinMode(LINwakePIN,INPUT);
+  digitalWrite(LINcsPIN,HIGH);
+  //digitalWrite(LINwakePIN,HIGH);
+  Serial1.begin(19200);
+  Serial.println("Started LIN at 19200.");
+  
+  Serial.println("Finished Starting Up... Type a command:");
+ 
+  k=0;
+  firstLINtime =true;
+  Serial1.flush();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -173,6 +252,107 @@ void loop(){
     int nDataBytes = Serial.readBytesUntil(sss.separatorChar,sss.command,99);
     sss.processCommand(nDataBytes);
   }
+  
+  
+  currentMicros = micros();
+
+//  if (Serial1.available()>=1){
+//        byte dataByte = Serial1.read();
+//        Serial.print(currentMicros-previousMicros);
+//        previousMicros = currentMicros;
+//        Serial.print(" 0x");
+//        Serial.println(dataByte,HEX);
+//      }
+//      
+//  else {
+  if (Serial1.available()>=3) 
+  {
+    byte firstChar = Serial1.read();
+    byte secondChar = Serial1.read() ;
+    byte thirdChar = Serial1.read() ;
+    if (firstChar == 0x00 && secondChar == 0xF0){
+      Serial.println("LIN Start");
+      Serial1.write(0xF0);
+    }
+    else if (firstChar == 0x00 && secondChar == 0x55 && thirdChar == 0x20 && firstLINtime){
+        firstLINtime =false;
+         Serial1.write(0xFF);
+         Serial1.write(0xF);
+         Serial1.write(0xFF);
+         Serial1.write(0x3F);
+         Serial1.write(0x91);
+         Serial.println("first LIN Pass");
+    }
+    else if (firstChar == 0x00 && secondChar == 0x55 && thirdChar == 0x20 && !firstLINtime)//Serial.print(micros());
+    {
+      outByte[0] = 0x14;
+      outByte[1] = (k << 4) + 0x01;
+      k+=1;
+      outByte[2] = 0x02;
+      outByte[3] = 0x0D;
+      
+      if      (outByte[1]==0x01) outByte[4] =0xBB;
+      else if (outByte[1]==0x11) outByte[4] =0xAB;
+      else if (outByte[1]==0x21) outByte[4] =0x9B;
+      else if (outByte[1]==0x31) outByte[4] =0x8B;
+      else if (outByte[1]==0x41) outByte[4] =0x7B;
+      else if (outByte[1]==0x51) outByte[4] =0x6B;
+      else if (outByte[1]==0x61) outByte[4] =0x5B;
+      else if (outByte[1]==0x71) outByte[4] =0x4B;
+      else if (outByte[1]==0x81) outByte[4] =0x3B;
+      else if (outByte[1]==0x91) outByte[4] =0x2B;
+      else if (outByte[1]==0xA1) outByte[4] =0x1B;
+      else if (outByte[1]==0xB1) outByte[4] =0x0B;
+      else if (outByte[1]==0xC1) outByte[4] =0xFA;
+      else if (outByte[1]==0xD1) outByte[4] =0xEA;
+      else if (outByte[1]==0xE1) outByte[4] =0xDA;
+      else if (outByte[1]==0xF1) outByte[4] =0xCA;
+      //else outByte[4] = -outByte[1] + 188;
+
+//      Serial1.write(outByte[0]);
+      serialSend0Micros = currentMicros+600;
+      LIN0send = true;
+      serialSend1Micros = currentMicros+1200;
+      LIN1send = true;
+      serialSend2Micros = currentMicros+1800;
+      LIN2send = true;
+      serialSend3Micros = currentMicros+2400;
+      LIN3send = true;
+      serialSend4Micros = currentMicros+3000;
+      LIN4send = true;
+      
+//      Serial.print("Sent ");
+//      for (int q = 0;q < 5;q++){
+//        
+//        Serial.print(" 0x");
+//        Serial.print(outByte[q],HEX);
+//      }
+//      Serial.println();
+    }
+  }    
+
+  if (currentMicros - serialSend0Micros >=0 && LIN0send) {
+    Serial1.write(outByte[0]);
+    LIN0send=false;
+  }
+  if (currentMicros - serialSend1Micros >=0 && LIN1send) {
+    Serial1.write(outByte[1]);
+    LIN1send=false;
+  }
+  if (currentMicros - serialSend2Micros >=0 && LIN2send) {
+    Serial1.write(outByte[2]);
+    LIN2send=false;
+  }
+  if (currentMicros - serialSend3Micros >=0 && LIN3send){
+    Serial1.write(outByte[3]);
+    LIN3send=false;
+  }
+  if (currentMicros - serialSend4Micros >=0 && LIN4send) {
+    Serial1.write(outByte[4]);
+    LIN4send=false;
+  }
+  //}
+  
   
   //Transmit periodic CAN messages that are stored in memory.
   sss.sendCANmessages();
